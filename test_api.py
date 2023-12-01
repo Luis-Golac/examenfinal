74310d1580c8e6412f67a03bcba3c40fbfdc2112
Luis-Golac <luis.golac@utec.edu.pe>
2023-11-30 20:55:56 -0500

    test

--- test_api.py
+++ test_api.py
@@ -1,9 +1,8 @@
 import pytest
-from myapp import create_app
+from app import app
 
 @pytest.fixture
 def client():
-    app = create_app()
     app.config['TESTING'] = True
 
     with app.test_client() as client:
@@ -17,4 +16,22 @@ def test_contactos_exitoso(client):
 def test_contactos_fallo_422(client):
     response = client.get('/billetera/contactos')
     assert response.status_code == 422
+
+def test_pagar_exitoso(client):
+    response = client.get('/billetera/pagar?minumero=123456789&numerodestino=123456787&valor=100')
+    assert response.status_code == 200
+    assert response.get_json().get('success') == True
+
+def test_pagar_fallo_406(client):
+    response = client.get('/billetera/pagar?minumero=123456789&numerodestino=123456789&valor=100')
+    assert response.status_code == 406
+
+def test_pagar_fallo_422(client):
+    response = client.get('/billetera/pagar?minumero=')
+    assert response.status_code == 422
+
+def test_historial_exitoso(client):
+    response = client.get('/billetera/historial?minumero=123456789')
+    assert response.status_code == 200
+    assert 'datos' in response.get_json()
 
